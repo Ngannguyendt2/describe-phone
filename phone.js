cb853b7b33aa5e7dfd55b820f41ff3e3bbc81e58
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-20 15:53:50 +0700

    show message

--- phone.js
+++ phone.js
@@ -1,5 +1,5 @@
 let Mobile = function (name, battery) {
-    const minbattery=10;
+    const minbattery = 10;
     this.name = name;
     this.battery = battery;
     this.message = '';
@@ -14,22 +14,20 @@ let Mobile = function (name, battery) {
         return this.battery;
     };
 
-    this.decreaseEnergy=function () {
-        if(this.battery>0)
-        {
+    this.decreaseEnergy = function () {
+        if (this.battery > 0) {
             this.battery--;
         }
     };
 
-    this.chargebattery=function () {
-         this.battery++;
-         alert(this.name+' charging'+ this.battery);
+    this.chargebattery = function () {
+        this.battery++;
+        alert(this.name + ' charging' + this.battery);
 
     };
 
     this.writeMsg = function (msg) {
         this.message = msg;
-        this.decreaseEnergy();
     };
 
     this.sendMsg = function (mobile) {
@@ -37,52 +35,48 @@ let Mobile = function (name, battery) {
             this.outbox.push(this.message);
             mobile.inbox.push(this.message);
         } else {
-            alert(this.name+" turned off");
+            alert(this.name + " turned off");
         }
         this.decreaseEnergy();
     };
 
-    this.showMsgInbox=function () {
-        
-    };
-
-    this.showMsgOutbox=function () {
-
-    };
-
     this.turnOnphone = function () {
         if (this.getBattery() >= minbattery) {
-            alert(this.name+" is turn on");
+            alert(this.name + " is turn on");
         } else {
-            alert(this.name+" is turn on but low battery");
+            alert(this.name + " is turn on but low battery");
         }
         this.decreaseEnergy();
         this.status = true;
     };
 
     this.turnOffphone = function () {
-        alert(this.name+" is turn off");
+        alert(this.name + " is turn off");
         this.status = false;
     };
 
     this.checkOnOff = function () {
         if (this.status) {
-            alert(this.name+" had being turn on");
+            alert(this.name + " had being turn on");
         } else {
-            alert(this.name+" had being turn off");
+            alert(this.name + " had being turn off");
         }
         this.decreaseEnergy();
     };
 
 }
 
-let iphone=new Mobile("IphoneXS",100);
-let nokia=new Mobile("Lumia525",100);
+let iphone = new Mobile("Iphone", 100);
+let nokia = new Mobile("Nokia", 100);
 
-function sendMessage(phone1,phone2) {
-let mobile1=document.getElementById('messageIphone').value;
-let mobile2=document.getElementById('messageNokia').value;
-phone1.writeMsg(mobile1);
-phone1.sendMsg(phone2);
+function sendMessage(mobile1,mobile2) {
+    mobile1.sendMsg(mobile2);
+    display(mobile1);
+    display(mobile2);
+}
+function display(mobile) {
+    document.getElementById("inbox"+mobile.name).innerHTML = mobile.inbox;
+    document.getElementById("outbox"+mobile.name).innerHTML = mobile.outbox;
+    document.getElementById("message"+mobile.name).value = "";
 }
 
